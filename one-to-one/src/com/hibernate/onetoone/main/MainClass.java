946a71a37253354173e9e6ab453dab3d12789ee4
Ahmed-Camara <camarahmed123@gmail.com>
2021-05-21 16:41:09 +0600

    Hibernate : one to one mapping

--- one-to-one/src/com/hibernate/onetoone/main/MainClass.java
+++ one-to-one/src/com/hibernate/onetoone/main/MainClass.java
@@ -3,10 +3,16 @@ package com.hibernate.onetoone.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.hibernate.onetoone.entity.Customer;
 import com.hibernate.onetoone.entity.Customer_detail;
 
+/**
+ * ONE TO ONE MAPPING
+ * 		- unidirectional
+ * 		- bidirectional
+ * **/
 public class MainClass {
 	
 	public static void main(String []args) {
@@ -19,13 +25,13 @@ public class MainClass {
 		
 		Session session = sessionFactory.getCurrentSession();
 		
-		Customer customer = new Customer("Ahmed","Camara");
-		
-		Customer_detail c_detail = new Customer_detail("abidjan,CIV","ahmed@gmail.com","SWE");
-		
-		customer.setC_detail(c_detail);
-		
 		try {
+			// 1. Save customer
+			Customer customer = new Customer("Ahmed","Camara");
+			
+			Customer_detail c_detail = new Customer_detail("abidjan,CIV","ahmed@gmail.com","SWE");
+			
+			customer.setC_detail(c_detail);
 			
 			session.beginTransaction();
 			
@@ -33,8 +39,52 @@ public class MainClass {
 			
 			session.getTransaction().commit();
 		
+			//2. Retrieve customer to database
+			String id = "1";
+			
+			session.beginTransaction();
+			
+			Customer cust = session.get(Customer.class, id);
+			
+			System.out.println(cust);
+			
+			session.getTransaction().commit();
+			
+			//3. Delete customer from the database
+			
+			String customer_id = "1";
+			
+			session.beginTransaction();
+			
+			Customer cus = session.get(Customer.class, customer_id);
+			
+			if(cus == null) {
+				System.out.println("Customer with id:"+customer_id+" does not exist.");
+			}else {
+				session.delete(cus);
+			}
+			
+			session.getTransaction().commit();
+			
+			//4. Update customer information
+			
+			String cust_id = "2";
+			
+			session.beginTransaction();
+			
+			Query query = session.createQuery("update Customer c set c.first_name='idriss' where id=:id");
+			query.setParameter("id", cust_id);
+			int executed = query.executeUpdate();
+			
+			if(executed == 1) {
+				System.out.println("Executed successfully");
+			}else {
+				System.out.println("Not Executed");
+			}
+			session.getTransaction().commit();
+			
 		}catch(Exception ex) {
-			System.out.println(ex.getMessage());
+			System.out.println("Error message : "+ex.getMessage());
 		}
 		
 		
