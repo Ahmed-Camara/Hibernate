e222f115a5cdd5071e8bd21f63a903ff32b082d7
Ahmed-Camara <camarahmed123@gmail.com>
2023-10-11 11:45:02 +0100

    One to one mapping

--- one-to-one/src/main/java/org/mapping/App.java
+++ one-to-one/src/main/java/org/mapping/App.java
@@ -24,48 +24,31 @@ public class App implements CommandLineRunner{
     }
     @Override
     public void run(String... args) throws Exception {
-        System.out.println( "Saving instructor along with his details" );
-       // this.saveInstructor();
-        System.out.println( "Done with saving instructor along with his details" );
-        System.out.println( "****************************************" );
 
+        //1. Save in the database
+        this.saveInstructor();
+
+        //2. Retrieve from the database
         Integer id = 3;
-        System.out.println("Getting one instructor based on id");
         Instructor instructor = this.getInstructor(id);
         System.out.println(instructor);
-        System.out.println( "Done Getting one instructor based on id" );
-        System.out.println( "****************************************" );
     }
 
     public void saveInstructor(){
         InstructorDetail instructorDetail = new InstructorDetail();
-        instructorDetail.setId(103);
-        instructorDetail.setHobby("Reading");
-        instructorDetail.setYoutubeChannel("https://www.youtube.com/AlmamDiaby");
+        instructorDetail.setId(100);
+        instructorDetail.setHobby("Learning");
+        instructorDetail.setYoutubeChannel("https://www.youtube.com/AhmedCamara");
 
         Instructor instructor = new Instructor();
         instructor.setId(4);
-        instructor.setEmail("almamdiaby@gmail.com");
-        instructor.setFirst_name("Diaby");
-        instructor.setLast_name("Almam");
+        instructor.setEmail("camarahmed@gmail.com");
+        instructor.setFirst_name("Camara");
+        instructor.setLast_name("Ahmed");
         instructor.setInstructorDetail(instructorDetail);
 
         instructorService.saveInstructor(instructor);
 
-
-        InstructorDetail instructorDetail1 = new InstructorDetail();
-        instructorDetail1.setId(102);
-        instructorDetail1.setHobby("Swimming");
-        instructorDetail1.setYoutubeChannel("https://www.youtube.com/IsmailCamara");
-
-        Instructor instructor1 = new Instructor();
-        instructor1.setId(3);
-        instructor1.setEmail("camaraismail@gmail.com");
-        instructor1.setFirst_name("Camara");
-        instructor1.setLast_name("Ismail");
-        instructor1.setInstructorDetail(instructorDetail1);
-
-        instructorService.saveInstructor(instructor1);
     }
 
     public Instructor getInstructor(Integer id){
